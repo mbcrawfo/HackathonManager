5243d08976424c2db89b7319f8b8a3ca1e04ad7b
Michael Crawford <mbcrawfo@gmail.com>
2025-06-12 23:05:45 -0400

    Set up startup migrations

--- src/HackathonManager/Program.cs
+++ src/HackathonManager/Program.cs
@@ -1,8 +1,25 @@
+using System;
+using HackathonManager.Migrator;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var connectionString =
+    builder.Configuration.GetConnectionString("HackathonDb")
+    ?? throw new InvalidOperationException("Connection string 'HackathonDb' is not configured.");
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+if (app.Configuration.GetValue<bool>("MigrateOnStart"))
+{
+    using var scope = app.Services.CreateScope();
+    var loggerFactory = scope.ServiceProvider.GetRequiredService<ILoggerFactory>();
+    MigrationRunner.UpdateDatabase(connectionString, loggerFactory);
+}
 
 await app.RunAsync();
