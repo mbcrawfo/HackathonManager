d9bc1d825ef143ed8b7a1c1faf8c091b77f36ef8
Michael Crawford <mbcrawfo@gmail.com>
2025-06-15 11:15:01 -0400

    Code coverage in CI

--- src/hackathon-app/vitest.config.ts
+++ src/hackathon-app/vitest.config.ts
@@ -5,9 +5,13 @@ import viteConfig from "./vite.config";
 
 export default (env: ConfigEnv) => {
     const reporters = ["default", "junit"];
-
     if (process.env.GITHUB_ACTIONS) {
         reporters.push("github-actions");
+    }
+
+    const coverageReporters = ["text", "json", "html"];
+    if (process.env.GITHUB_ACTIONS) {
+        coverageReporters.push("json-summary");
     }
 
     return mergeConfig(
@@ -19,6 +23,10 @@ export default (env: ConfigEnv) => {
                 root: fileURLToPath(new URL("./", import.meta.url)),
                 reporters,
                 outputFile: "test-results/unit-tests.xml",
+                coverage: {
+                    reporter: coverageReporters,
+                    reportOnFailure: true,
+                },
             },
         }),
     );
