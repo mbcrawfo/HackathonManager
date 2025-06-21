d3334f8f1a2d899f6435cd77eef00622fc8bfafb
Michael Crawford <mbcrawfo@gmail.com>
2025-06-21 13:05:45 -0400

    Remove unneeded assembly attribute

--- tests/HackathonManager.Tests/AssemblyAttributes.cs
+++ tests/HackathonManager.Tests/AssemblyAttributes.cs
@@ -1,11 +1,4 @@
-using Microsoft.AspNetCore.Mvc.Testing;
 using Xunit;
 
 [assembly: CaptureConsole]
 [assembly: CaptureTrace]
-// [assembly: WebApplicationFactoryContentRoot(
-//     "HackathonManager.Program",
-//     "./IntegrationTests/wwwroot",
-//     "index.html",
-//     "1"
-// )]
