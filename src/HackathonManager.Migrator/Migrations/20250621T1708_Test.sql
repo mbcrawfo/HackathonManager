9a1b05bee55187e467fed9412fb22b3bcee08406
Michael Crawford <mbcrawfo@gmail.com>
2025-06-28 13:25:01 -0400

    Add sqlfluff linting

--- src/HackathonManager.Migrator/Migrations/20250621T1708_Test.sql
+++ src/HackathonManager.Migrator/Migrations/20250621T1708_Test.sql
@@ -1,6 +1,6 @@
 create table test (id uuid primary key, name text not null);
 
 insert into test (id, name) values
-    (uuidv7(), 'foo'),
-    (uuidv7(), 'bar'),
-    (uuidv7(), 'baz')
+(uuidv7(), 'foo'),
+(uuidv7(), 'bar'),
+(uuidv7(), 'baz')
--- src/HackathonManager.Migrator/Migrations/20250628T1213_TypeIdEqualityOperator.sql
+++ src/HackathonManager.Migrator/Migrations/20250628T1213_TypeIdEqualityOperator.sql
@@ -1,9 +1,10 @@
-create operator ===(
+-- Creates an equality operator for comparing a uuid and a typeid string.
+create operator ===( -- noqa: LT01
     leftarg = uuid,
     rightarg = text,
     function = uuid_typeid_eq_operator,
-    commutator = ===,
-    negator = !==,
+    commutator = ===, -- noqa: LT01
+    negator = !==, -- noqa: LT01
     hashes,
     merges
 );
