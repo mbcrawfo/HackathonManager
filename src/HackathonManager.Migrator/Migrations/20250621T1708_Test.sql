0f64da0ca5f159bdebf13c0620198e3536d7cebc
Michael Crawford <mbcrawfo@gmail.com>
2025-06-29 18:25:08 -0400

    Add database doc update on PR

--- src/HackathonManager.Migrator/Migrations/20250621T1708_Test.sql
+++ src/HackathonManager.Migrator/Migrations/20250621T1708_Test.sql
@@ -3,4 +3,5 @@ create table test (id uuid primary key, name text not null);
 insert into test (id, name) values
 (uuidv7(), 'foo'),
 (uuidv7(), 'bar'),
-(uuidv7(), 'baz')
+(uuidv7(), 'baz'),
+(uuidv7(), 'qux');
