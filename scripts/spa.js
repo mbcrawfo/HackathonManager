74d1f54b2e6e11b1e75929615a1dfc13490b39bf
Michael Crawford <mbcrawfo@gmail.com>
2025-06-29 13:30:59 -0400

    Move prettier to repo root

--- scripts/spa.js
+++ scripts/spa.js
@@ -1,16 +1,16 @@
-const { spawn } = require("child_process");
-const command = process.argv[2];
+import { spawn } from "child_process";
 
+const command = process.argv[2];
 if (!command) {
-  console.error("Please provide a command");
-  process.exit(1);
+    console.error("Please provide a command");
+    process.exit(1);
 }
 
 let args = [];
 if (process.argv.length > 3) {
-  args = process.argv.slice(3);
+    args = process.argv.slice(3);
 }
 
 spawn("npm", ["run", "--workspace", "src/hackathon-spa", command, "--", ...args], {
-  stdio: "inherit",
+    stdio: "inherit",
 });
